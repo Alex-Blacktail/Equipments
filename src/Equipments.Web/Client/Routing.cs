7413822c358f3f4751c3f191e69cf91754529fa5
Alexey Popov <a.popov@w1.money>
2022-11-21 19:40:38 +0500

    Добавил класс в клиенте для хранения ендпоинтов

---
+++ src/Equipments.Web/Client/Routing.cs
@@ -0,0 +1,13 @@
+﻿namespace Equipments.Web.Client
+{
+    internal static class Routing
+    {
+        public const string MeasureUnits = "MeasureUnits/";
+        public const string DataTypes = "DataTypes/";
+        public const string StateTypes = "StateTypes/";
+
+        public const string ComponentTypes = "ComponentTypes/";
+        public const string EquipmentTypes = "EquipmentTypes/";
+
+    }
+}
